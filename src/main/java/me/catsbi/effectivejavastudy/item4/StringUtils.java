98759b586c98556a0ac6ae9d8cb82bce58fa192a
catsbi <killfine1988@gmail.com>
2021-05-14 10:24:14 +0900

    feat: item4 -인스턴스화를 막으려거든 private 생성자를 사용하라

---
+++ src/main/java/me/catsbi/effectivejavastudy/item4/StringUtils.java
@@ -0,0 +1,21 @@
+package me.catsbi.effectivejavastudy.item4;
+
+public abstract class StringUtils {
+
+    //인스턴스화 방지 생성자.
+    private StringUtils() {
+        throw new AssertionError();
+    }
+
+    public static void print(String message) {
+        System.out.println(message);
+    }
+}
+
+/*StringUtils의 기본생성자가 private 생성자가 되며 상속이 불가능해진다.*/
+/*public class StringUtilsChild extends StringUtils{
+
+    public StringUtilsChild() {
+        super();
+    }
+}*/
