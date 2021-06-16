96e65abbeb92ab7433ef9cf114bfd0c95f2b0548
catsbi <killfine198@gmail.com>
2021-06-16 09:16:10 +0900

    feat: item22 - 인터페이스는 타입을 정의하는 용도로만 사용하라

---
+++ src/main/java/me/catsbi/effectivejavastudy/chapter3/item22/PhysicalConstants.java
@@ -0,0 +1,9 @@
+package me.catsbi.effectivejavastudy.chapter3.item22;
+
+public class PhysicalConstants {
+		private PhysicalConstants() { } //인스턴스화 방지
+		
+		public static final double AVOGADROS_NUMBER = 6.022_140_857e23;
+		public static final double BOLTZMAN_CONSTANT = 1.380_648_52e-23;
+		public static final double ELECTRON_MASS = 9.109_383_56e-31;
+}
---
+++ src/main/java/me/catsbi/effectivejavastudy/chapter3/item22/PhysicalConstantsIsAntiPattern.java
@@ -0,0 +1,7 @@
+package me.catsbi.effectivejavastudy.chapter3.item22;
+
+public interface PhysicalConstantsIsAntiPattern {
+		static final double AVOGADROS_NUMBER = 6.022_140_857e23;
+		static final double BOLTZMANN_CONSTANT = 1.380_648_52e-23;
+		static final double ELECTRON_MASS = 9.109_383_56e-31;
+}
